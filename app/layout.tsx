e9cc57fee0c0ba055fe00ab42f72f53b121398d8
Rémi Lux <remrem2257@gmail.com>
2023-11-29 14:49:07 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rémi Lux",
+  description:
+    "The best full-stack developer in France (and maybe in the world)",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
