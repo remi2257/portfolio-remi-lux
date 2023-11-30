3665a4b839e4d1d28c5c45ef33a2663bbc94d74d
Rémi Lux <remrem2257@gmail.com>
2023-11-30 10:54:28 +0100

    Add Tooltip to skills

--- app/About.tsx
+++ app/About.tsx
@@ -1,19 +1,51 @@
+import { IconType } from "react-icons";
 import {
   SiAngularjs,
+  SiCplusplus,
   SiCss3,
   SiDocker,
+  SiFirebase,
+  SiGit,
   SiHtml5,
   SiJavascript,
   SiNextdotjs,
+  SiNodedotjs,
   SiPostgresql,
+  SiPython,
   SiReact,
   SiSupabase,
   SiTailwindcss,
   SiTypescript,
+  SiVercel,
 } from "react-icons/si";
 
 import SectionTitle from "@/components/atoms/SectionTitle";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
+
+const SkillIcon: React.FC<{
+  Icon: IconType;
+  tooltip: string;
+  className?: string;
+}> = ({ Icon, tooltip, className }) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Icon
+            className={cn("h-10 w-10 sm:h-12 sm:w-12 transition", className)}
+          />
+        </TooltipTrigger>
+        <TooltipContent>{tooltip}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
 
 const About: React.FC<{ index: number; className?: string }> = ({
   index,
@@ -21,7 +53,6 @@ const About: React.FC<{ index: number; className?: string }> = ({
 }) => {
   const skillLineClasses =
     "flex flex-wrap justify-center items-center gap-3 sm:gap-x-4";
-  const iconClasses = "h-10 w-10 sm:h-12 sm:w-12";
 
   return (
     <section id="about" className={cn("max-w-prose space-y-8", className)}>
@@ -51,42 +82,98 @@ const About: React.FC<{ index: number; className?: string }> = ({
         </div>
         <div className="flex flex-col items-center gap-6 sm:gap-8">
           <div className={skillLineClasses}>
-            <SiHtml5
-              className={cn(iconClasses, "transition hover:text-orange-500")}
+            <SkillIcon
+              Icon={SiHtml5}
+              tooltip="HTML"
+              className={"hover:text-orange-500"}
             />
-            <SiCss3
-              className={cn(iconClasses, "transition hover:text-blue-500")}
+            <SkillIcon
+              Icon={SiCss3}
+              tooltip="CSS"
+              className={"hover:text-blue-500"}
             />
-            <SiTailwindcss
-              className={cn(iconClasses, "transition hover:text-teal-500")}
+            <SkillIcon
+              Icon={SiTailwindcss}
+              tooltip="TailwindCSS"
+              className={"hover:text-teal-500"}
             />
-            <SiJavascript
-              className={cn(iconClasses, "transition hover:text-yellow-400")}
+            <SkillIcon
+              Icon={SiJavascript}
+              tooltip="JavaScript"
+              className={"hover:text-yellow-400"}
             />
-            <SiTypescript
-              className={cn(iconClasses, "transition hover:text-blue-700")}
+            <SkillIcon
+              Icon={SiTypescript}
+              tooltip="TypeScript"
+              className={"hover:text-blue-700"}
             />
           </div>
           <div className={skillLineClasses}>
-            <SiReact
-              className={cn(iconClasses, "transition hover:text-blue-400")}
+            <SkillIcon
+              Icon={SiReact}
+              tooltip="React.js"
+              className={"hover:text-blue-400"}
             />
-            <SiNextdotjs
-              className={cn(iconClasses, "transition hover:text-neutral-500")}
+            <SkillIcon
+              Icon={SiNextdotjs}
+              tooltip="Next.js"
+              className={"hover:text-neutral-500"}
             />
-            <SiAngularjs
-              className={cn(iconClasses, "transition hover:text-red-600")}
+            <SkillIcon
+              Icon={SiAngularjs}
+              tooltip="Angular"
+              className={"hover:text-red-600"}
             />
           </div>
           <div className={skillLineClasses}>
-            <SiSupabase
-              className={cn(iconClasses, "transition hover:text-green-600")}
+            <SkillIcon
+              Icon={SiSupabase}
+              tooltip="Supabase"
+              className={"hover:text-green-600"}
             />
-            <SiPostgresql
-              className={cn(iconClasses, "transition hover:text-blue-800")}
+            <SkillIcon
+              Icon={SiPostgresql}
+              tooltip="PostgreSQL"
+              className={"hover:text-blue-800"}
             />
-            <SiDocker
-              className={cn(iconClasses, "transition hover:text-sky-500")}
+            <SkillIcon
+              Icon={SiFirebase}
+              tooltip="Firebase"
+              className={"hover:text-amber-500"}
+            />
+          </div>
+          <div className={skillLineClasses}>
+            <SkillIcon
+              Icon={SiGit}
+              tooltip="Git"
+              className={"hover:text-red-500"}
+            />
+            <SkillIcon
+              Icon={SiVercel}
+              tooltip="Vercel"
+              className={"hover:text-neutral-500"}
+            />
+            <SkillIcon
+              Icon={SiDocker}
+              tooltip="Docker"
+              className={"hover:text-sky-500"}
+            />
+          </div>
+          <div className={skillLineClasses}>
+            <SkillIcon
+              Icon={SiNodedotjs}
+              tooltip="Node.js"
+              className={"hover:text-green-600"}
+            />
+            <SkillIcon
+              Icon={SiPython}
+              tooltip="Python"
+              className={"hover:text-yellow-500"}
+            />
+            <SkillIcon
+              Icon={SiCplusplus}
+              tooltip="C++"
+              className={"hover:text-blue-700"}
             />
           </div>
         </div>
