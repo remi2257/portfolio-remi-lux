812dc38f647810256f0e98ff8d89c47bb38e6efc
Rémi Lux <remrem2257@gmail.com>
2023-11-30 10:55:07 +0100

    Add tools to Footer

--- components/molecules/Footer.tsx
+++ components/molecules/Footer.tsx
@@ -1,10 +1,17 @@
+import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
+
 import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
+  const iconClasses = "inline-block h-6 w-6 mx-2";
+
   return (
     <footer className="mt-8 border-t sm:mt-12">
-      <div className="container flex flex-col items-center justify-center gap-y-2 py-4 sm:flex-row sm:justify-between">
-        <h2 className="text-center font-bold">Developped by Rémi Lux</h2>
+      <div className="container flex flex-col items-center justify-center gap-y-4 py-4 sm:flex-row sm:justify-between">
+        <h2 className="text-center font-bold">
+          Developped by Rémi Lux with <SiNextdotjs className={iconClasses} /> &{" "}
+          <SiTailwindcss className={iconClasses} />
+        </h2>
         <SocialIcons />
       </div>
     </footer>
